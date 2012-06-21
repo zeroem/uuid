07730154e83501980de0c182ad15e5f22e655134
Darrell Hamilton <darrell.noice@gmail.com>
2012-06-20 23:54:30 -0700

    added counter for multiple requests made in the same microsecond and only generate a random sequence once and increment after that

--- src/Zeroem/UUID/UUID.php
+++ src/Zeroem/UUID/UUID.php
@@ -12,13 +12,16 @@ class UUID
     $clock_seq_low,
     $node;
 
+  static $subMicrosecondCount=0;
+  static $sequence = null;
+
   public function __construct($node) {
     $timestamp = self::getUTCTimeStamp();
     $this->time_low = self::convert($timestamp & 0xffffffff);
     $this->time_mid = self::convert(($timestamp >> 32) & 0x000000ff);
     $this->time_hi_and_version = self::convert((($timestamp >> 48) & 0x0fffffff) | 0x10000000);
 
-    $sequence = mt_rand(0,0xffff);
+    $sequence = self::getSequence();
     $this->clock_seq_low = self::convert($sequence & 0xff);
     $this->clock_seq_hi_and_reserved = self::convert(($sequence >> 8) & 0x1f | 0x8);
 
@@ -42,10 +45,23 @@ class UUID
    */
   static function getUTCTimestamp() {
     list($ms,$sec) = explode(' ', microtime());
-    return ((($sec - gmmktime(0,0,0,10,15,1582)) * 1000) + $ms) * 10;
+    $value = ((($sec - gmmktime(0,0,0,10,15,1582)) * 1000) + $ms + (self::$subMicrosecondCount)) * 10;
+    self::$subMicrosecondCount = (self::$subMicrosecondCount+1) % 10;
+
+    return $value;
   }
 
   static function convert($number) {
     return base_convert($number,10,16);
+  }
+
+  static private function getSequence() {
+    if(!isset(self::$sequence)) {
+      self::$sequence = mt_rand(0,0xffff);
+    } else {
+      self::$sequence++;
+    }
+    
+    return self::$sequence;
   }
 }
